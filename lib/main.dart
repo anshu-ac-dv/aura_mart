7a50317fd0f702bc3201cd1e708b65407440920d
anshu <anshu.ac.dv@gmail.com>
2026-03-16 04:58:52 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
-import 'package:aura_mart/Splash%20Services/Splash_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'Splash Services/Splash_Screen.dart';
 import 'firebase_options.dart';
 
-// Entry point of the application
 void main() async {
-  // Ensure Flutter bindings are initialized before calling asynchronous code
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Initialize Firebase with platform-specific options
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
-  // Run the root widget of the app
   runApp(const MyApp());
 }
 
@@ -25,11 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Aura Mart',
-      
-      // Theme settings to support Light and Dark modes automatically based on system settings
       themeMode: ThemeMode.system, 
-      
-      // Light Theme Configuration
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
@@ -37,8 +27,6 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      
-      // Dark Theme Configuration
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
@@ -46,8 +34,6 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      
-      // Initial screen to show when the app starts
       home: const SplashScreen(),
     );
   }
