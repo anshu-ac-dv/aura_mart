c93fd4097bb25e3bfe8279f65107105f2def0554
anshu <anshu.ac.dv@gmail.com>
2026-03-18 13:20:24 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aura_mart/Screens/Splash/splash_screen.dart';
+import 'package:aura_mart/Splash%20Services/Splash_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
