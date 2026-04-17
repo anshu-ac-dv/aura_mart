8b9ad82d2f21cc241e150479aff541260485dbbc
anshu <anshu.ac.dv@gmail.com>
2026-04-17 21:05:11 +0530

    Initial Commit

--- lib/Screens/Tabs/category_tab.dart
+++ lib/Screens/Tabs/category_tab.dart
@@ -155,7 +155,7 @@ class _CategoryTabState extends State<CategoryTab> {
                 shape: BoxShape.circle,
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
+                    color: Colors.black.withValues(alpha: 0.05),
                     blurRadius: 10,
                     offset: const Offset(0, 5),
                   )
